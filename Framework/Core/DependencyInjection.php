71cf30a67e36e2611268a772cd094550cae5ed01
tayueliuxiang1@sina.com <tayueliuxiang1@sina.com>
2019-06-21 14:52:08 +0800

    fix

---
+++ Framework/Core/DependencyInjection.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * php利用反射特性模拟java类依赖注入
+ */
+
+namespace Framework\Core;
+
+use Framework\Traits\ContainerTrait;
+use Framework\Traits\SingletonTrait;
+use Framework\SwServer\ServerManager;
+
+class DependencyInjection
+{
+    public static function make($className, $methodName, $params = [])
+    {
+        // 获取类的实例
+        $instance = self::getInstance($className);
+        // 获取该方法所需要依赖注入的参数
+        $paramArr = self::resolveClassMethodDependencies($className, $methodName);
+        return $instance->{$methodName}(...array_merge($paramArr, $params));
+    }
+
+    public static function getInstance($className)
+    {
+        $paramArr = self::resolveClassMethodDependencies($className);
+        return ServerManager::getApp()->registerObject($className, ['class' => $className], $paramArr);
+    }
+
+    public static function resolveClassMethodDependencies($className, $method = '__construct')
+    {
+        $parameters = []; // 记录参数，和参数类型
+        if (!\method_exists($className, $method)) {
+            return $parameters;
+        }
+        // 获得构造函数
+        $reflector = new \ReflectionMethod($className, $method);
+        if (count($reflector->getParameters()) <= 0) {
+            return $parameters;
+        }
+        foreach ($reflector->getParameters() as $key => $parameter) {
+            $currentParamsReflectionClass = $parameter->getClass();
+            if ($currentParamsReflectionClass) {
+                // 获得参数类型名称
+                $paramClassName = $currentParamsReflectionClass->getName();
+                $paramClassParams = self::resolveClassMethodDependencies($paramClassName);
+                $parameters[] = ServerManager::getApp()->registerObject($paramClassName, ['class' => $paramClassName], $paramClassParams);
+            }
+
+        }
+        return $parameters;
+    }
+
+
+}
+
